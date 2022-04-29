5497c9899342e931099762198dba6c3633218f7e
Nakaya <eniehack@outlook.jp>
2022-04-29 19:38:28 +0900

    fix: unuse ulid.ULID for model

--- internal/model/posts.go
+++ internal/model/posts.go
@@ -3,14 +3,13 @@ package model
 import (
 	"time"
 
-	"github.com/oklog/ulid/v2"
 	"gorm.io/gorm"
 )
 
 type Posts struct {
-	ID ulid.ULID `gorm:"primaryKey;<-:create;type:char(26);uniqueIndex;"`
+	ID string `gorm:"primaryKey;<-:create;type:char(26);uniqueIndex;"`
 	Title string `gorm:"type:varchar(20);"`
-	AuthorID ulid.ULID `gorm:"uniqueIndex"`
+	AuthorID string `gorm:"uniqueIndex"`
 	Author Users `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:AuthorID;"`
 	IsDraft bool `gorm:"default:false;"`
 	Scope uint `gorm:"type:unsigned;"`
--- internal/model/users.go
+++ internal/model/users.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"crypto/rand"
 	"time"
 
 	"github.com/oklog/ulid/v2"
@@ -8,12 +9,23 @@ import (
 )
 
 type Users struct {
-	Ulid ulid.ULID `gorm:"primaryKey;<-:create;type:char(26);uniqueIndex"`
-	UserID string `gorm:"unique;type:varchar(15);uniqueIndex;not null"`
+	Ulid string `gorm:"primaryKey;<-:create;type:char(26);uniqueIndex"`
+	UserId string `gorm:"unique;type:varchar(15);uniqueIndex;not null"`
 	Name string `gorm:"size:20;not null"`
 	Password string `gorm:"not null;type:char(97)"`
 	IsAdmin bool `gorm:"default:false"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
+}
+
+func (u *Users) BeforeCreate(db *gorm.DB) error {
+	if u.Ulid == "" {
+		ulid, err := ulid.New(ulid.Now(), rand.Reader)
+		if err != nil {
+			return err
+		}
+		u.Ulid = ulid.String()
+	}
+	return nil
 }
